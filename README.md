f1f107619bd892a0e5ff8f9639330f99afaa3005
Joël Grimberg <jgrimberg@xebia.com>
2019-04-12 15:36:52 +0200

    add client and server

---
+++ README.md
@@ -0,0 +1,37 @@
+- Forked from git@github.com:codyseibert/tab-tracker.git
+- run `npm start` in website folder to view documentation for the workshop 
+
+
+
+# Tab Tracker
+A Vue.js / Express.js web application for keeping track of guitar tabs
+
+# Setup
+
+You need to have Node Js installed: https://nodejs.org/en/
+
+You must at least have **NODE version 8.2.1**
+
+Don't know how to install different version of node?  Try this: https://github.com/tj/n
+
+I suggest you install the following that I use in the video:
+
+VSCode: https://code.visualstudio.com/
+
+ITerm2: https://www.iterm2.com/ (Mac Only)
+
+Git: https://git-scm.com/
+
+### Client - Terminal A
+```
+cd client
+npm install
+npm start
+```
+
+### Server - Terminal B
+```
+cd server
+npm install
+npm start
+```
---
+++ client/src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <router-link 
+        class="home"
+        tag="span"
+        :to="{
+          name: 'songs'
+        }">
+        TabTracker
+      </router-link>
+    </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn 
+        flat 
+        dark
+        :to="{
+          name: 'songs'
+        }">
+        Browse
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn 
+        v-if="!$store.state.isUserLoggedIn"
+        flat 
+        dark
+        :to="{
+          name: 'login'
+        }">
+        Login
+      </v-btn>
+      
+      <v-btn 
+        v-if="!$store.state.isUserLoggedIn"
+        flat 
+        dark
+        :to="{
+          name: 'register'
+        }">
+        Sign Up
+      </v-btn>
+      
+      <v-btn 
+        v-if="$store.state.isUserLoggedIn"
+        flat 
+        dark
+        @click="logout">
+        Log Out
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'songs'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  cursor: pointer;
+}
+
+.home:hover {
+  color: #E9E;
+}
+</style>
